99b4a575505c740d78cad582db8a1b3c5247c726
lauriekeren <lauriekeren@gmail.com>
2019-07-03 09:52:37 -0400

    Add files via upload

--- display_results.php
+++ display_results.php
@@ -1,181 +1,195 @@
 <?php
-include 'includes/functions.php';
-// get the data from the form
-$loan_amount = filter_input(INPUT_POST, 'loan_amount',
-                            FILTER_VALIDATE_FLOAT);
-$interest_rate = filter_input(INPUT_POST, 'interest_rate',
-                              FILTER_VALIDATE_FLOAT);
-$loan_length = filter_input(INPUT_POST, 'loan_length',
-                            FILTER_VALIDATE_INT);
-$time_unit = filter_input(INPUT_POST, 'time_unit');
-$extra_payment = filter_input(INPUT_POST, 'extra_payment',
-                              FILTER_VALIDATE_FLOAT);
-// validate Loan amount
-validate($error_message, $loan_amount, $interest_rate, $loan_length, $time_unit);
-// if an error message exists, go to the index page
-if ($error_message != '') {
-    include'index.php';
-    exit(); }
-//make sure extra payment is not a negative number
-if ($extra_payment < 0){
-$extra_payment = 0;
-}
-//initialize the monthly interest rate
-$interest = calc_monthly_int($interest, $interest_rate);
-//make sure the loan length is in months and not years
-loan_months($time_unit, $loan_length);
-//create a time stamp for current date
-$date = new DateTime('now');
-//calculate the monthly payment
-$monthly_payment = $loan_amount * (($interest * pow($interest + 1, $loan_length)) / (pow($interest + 1, $loan_length) - 1));
-//calculate the total interest paid when there is no extra payment
-$total_interest = ($monthly_payment * ($loan_length)) - $loan_amount;
+  ini_set('display_errors', 1);
 
-//format for the summary table
-$loan_amount_f = '$'. number_format($loan_amount, 2);
-$monthly_payment_f = '$' . number_format($monthly_payment, 2);
-$extra_payment_f = '$' . number_format($extra_payment, 2);
-$interest_rate_f = $interest_rate.'%'
+/* functions file */
+  include 'includes/functions.php'; 
+
+// get the data from the form
+   $loan_amount = filter_input(INPUT_POST, 'loan_amount',
+                            FILTER_VALIDATE_FLOAT);
+   $interest_rate = filter_input(INPUT_POST, 'interest_rate',
+                              FILTER_VALIDATE_FLOAT);
+   $loan_length = filter_input(INPUT_POST, 'loan_length',
+                            FILTER_VALIDATE_INT);
+   $time_unit = filter_input(INPUT_POST, 'time_unit');
+   $extra_payment = filter_input(INPUT_POST, 'extra_payment',
+                              FILTER_VALIDATE_FLOAT);
+                                      
+// validate Loan amount
+   validate($error_message, $loan_amount, $interest_rate, $loan_length, $time_unit);
+
+
+// if an error message exists, go to the index page
+   if ($error_message != '') {
+       include 'index.php';
+       exit(); }
+    
+    
+//make sure extra payment is not a negative number
+   if ($extra_payment < 0){
+       $extra_payment = 0;
+   }
+
+
+//initialize the monthly interest rate
+   $interest = calc_monthly_int($interest_rate);
+
+//if the loan is entered as years, convert to months
+   loan_months($time_unit, $loan_length);
+
+
+//create a time stamp for current date
+   $date = new DateTime('now');
+
+
+//calculate the monthly payment using A = P((r(1+r)^n)/((1+r)^n - 1))
+   $monthly_payment = $loan_amount * (($interest * pow($interest + 1, $loan_length)) / (pow($interest + 1, $loan_length) - 1));
+
+
+//calculate the total interest paid when there is no extra payment
+   $total_interest = ($monthly_payment * ($loan_length)) - $loan_amount;
+
+
+//format variables for the summary table
+   $loan_amount_f = '$'. number_format($loan_amount, 2);
+   $monthly_payment_f = '$' . number_format($monthly_payment, 2);
+   $extra_payment_f = '$' . number_format($extra_payment, 2);
+   $interest_rate_f = $interest_rate.'%';
+   $months_saved_f = $loan_length;
+
+include 'includes/header.php';
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>
-            Loan Amortization Calculator
-        </title>
-        <link rel="stylesheet" type="text/css" href="styles/main.css">
-    </head>
-    <body>
-        <header>
-            <img src="img/loancalc.png" alt="Loan Calculator">
-        </header>
         <main class="grid">
             <div class="box">
-                <img src="img/amortizationschedule.png" alt="Amortization Schedule" height="50px">
+                <img src="img/amort_sched/amortizationschedule.png" alt="Amortization Schedule" height="40">
                 <div id="grid_amortization">
                     <div>
-                        Pmt
-                        <br>
-                        No.
+                        <img src="img/amort_sched/pmtno.png" alt="payment number">
                     </div>
                     <div>
-                        Due
-                        <br>
-                        m-d-y
+                        <img src="img/amort_sched/duedate.png" alt="due date">
                     </div>
                     <div>
-                        Monthly
-                        <br>
-                        Payment
+                        <img src="img/amort_sched/monthlypayment1.png" alt="monthlypayment">
                     </div>
                     <div>
-                        Payment
-                        <br>
-                        Interest
+                        <img src="img/amort_sched/paymentinterest.png" alt="payment interest">
                     </div>
                     <div>
-                        Payment
-                        <br>
-                        Principle
+                        <img src="img/amort_sched/paymentprinciple.png" alt="payment principle">
                     </div>
                     <div>
-                        Extra
-                        <br>
-                        Principle
+                        <img src="img/amort_sched/extraprinciple.png" alt="extra principle">
                     </div>
                     <div>
-                        Loan
-                        <br>
-                        Balance
+                        <img src="img/amort_sched/loanbalance.png" alt="loan balance">
                     </div>
                     <?php 
-//Set the remaining balance variable
-$remaining_balance = $loan_amount;
+                    
+//Set a remaining balance variable and a variable for adding the interest
+  $remaining_balance = $loan_amount;
+  $full_interest = 0;
 
 //This for loop will calculate all of the variables that will populate the table.
 //It will take into account the extra payment option and calculate the variations
 //that will occur when the last payment is reached.
-for ($i = 1; $i <= $loan_length; $i++) {
-    if ($monthly_payment > ($remaining_balance * $interest) + $remaining_balance){
-        $monthly_interest = $remaining_balance * $interest;
-        $principle = $remaining_balance;
-        $monthly_payment = $remaining_balance + $monthly_interest;
-        $extra_payment = 0;
-        $remaining_balance = 0;
-        $full_interest += $monthly_interest;
-        display_table($date, $i, $monthly_payment, $monthly_interest, $principle, $extra_payment, $remaining_balance);
-        break;
-    }elseif($monthly_payment + $extra_payment > ($remaining_balance * $interest) + $remaining_balance ) {
-        $monthly_interest = $remaining_balance * $interest;
-        $principle = $monthly_payment - $monthly_interest;
-        $extra_payment = $remaining_balance - $monthly_payment;
-        $remaining_balance = 0;
-        $full_interest += $monthly_interest;
-        display_table($date, $i, $monthly_payment, $monthly_interest, $principle, $extra_payment, $remaining_balance);
-        break;
-    } 
-    $monthly_interest = $remaining_balance * $interest;
-    $principle = $monthly_payment - $monthly_interest;
-    $remaining_balance = $remaining_balance - $principle - $extra_payment;
-    $full_interest += $monthly_interest;
-    display_table($date, $i, $monthly_payment, $monthly_interest, $principle, $extra_payment, $remaining_balance);
-}
-if ($i > $loan_length){
-	$i = $loan_length;
-}
-//format for the summary table
-$full_interest_f = '$' . number_format($full_interest, 2);
-$total_amt_f = '$' . number_format($loan_amount + $full_interest, 2);
-$interest_saved_f = '$' . number_format(abs($total_interest - $full_interest), 2);
-                     ?>  
+   for ($i = 1; $i <= $loan_length; $i++) {
+   
+    //if there is an extra payment, these two options will calculate the final numbers for the last payment
+    //taking into account whether or not the extra payment is needed for that final payment
+       if ($monthly_payment > ($remaining_balance * $interest) + $remaining_balance){
+           $monthly_interest = $remaining_balance * $interest;
+           $principle = $remaining_balance;
+           $monthly_payment = $remaining_balance + $monthly_interest;
+           $extra_payment = 0;
+           $remaining_balance = 0;
+           $full_interest += $monthly_interest;
+           display_table($date, $i, $monthly_payment, $monthly_interest, $principle, $extra_payment, $remaining_balance);
+           break;
+           
+       }elseif($monthly_payment + $extra_payment > ($remaining_balance * $interest) + $remaining_balance ) {
+           $monthly_interest = $remaining_balance * $interest;
+           $principle = $monthly_payment - $monthly_interest;
+           $extra_payment = $remaining_balance - $monthly_payment;
+           $remaining_balance = 0;
+           $full_interest += $monthly_interest;
+           display_table($date, $i, $monthly_payment, $monthly_interest, $principle, $extra_payment, $remaining_balance);
+           break;
+       } 
+       
+    //if there is no extra payment, this will populate the complete summary table
+       $monthly_interest = $remaining_balance * $interest;
+       $principle = $monthly_payment - $monthly_interest;
+       $remaining_balance = $remaining_balance - $principle - $extra_payment;
+       $full_interest += $monthly_interest;
+       display_table($date, $i, $monthly_payment, $monthly_interest, $principle, $extra_payment, $remaining_balance);
+   }
+   
+//because of the way the for loop is structured, if there is an extra payment, the loan length could be off by one
+//this corrects that problem
+   if ($i > $loan_length){
+       $i = $loan_length;
+   }
+   
+//format variables for the summary table
+   $full_interest_f = '$' . number_format($full_interest, 2);
+   $total_amt_f = '$' . number_format($loan_amount + $full_interest, 2);
+   $interest_saved_f = '$' . number_format(abs($total_interest - $full_interest), 2);
+   $months_saved_f -= $i;
+                    ?>  
                 </div>
             </div>
             <div class="box">
-                <img src="img/loansummary.png" alt="Loan Summary" height="50px">
+                <img src="img/loan_summary/loansummary.png" alt="Loan Summary" height="40">
                 <div id="grid_summary">
                     <div class="1">
-                        Loan Amount:
+                        <img src="img/loan_summary/loanamount.png" alt="loan amount">
                     </div>
                     <div class="2">
                         <?php echo $loan_amount_f; ?></div>
                     <div>
-                        Yearly Interest Rate:
+                        <img src="img/loan_summary/interestrate.png" alt="interest rate">
                     </div>
                     <div>
                         <?php echo $interest_rate_f; ?></div>
                     <div>
-                        Number of Months:
+                        <img src="img/loan_summary/loanmonths.png" alt="number of months">
                     </div>
                     <div>
                         <?php echo $i; ?></div>
                     <div>
-                        Monthly Payment:
+                        <img src="img/loan_summary/monthlypayment.png" alt="monthly payment">
                     </div>
                     <div>
                         <?php echo $monthly_payment_f; ?></div>
                     <div>
-                        Total Interest:
+                        <img src="img/loan_summary/totalinterest.png" alt="total interest">
                     </div>
                     <div>
                         <?php echo $full_interest_f; ?></div>
                     <div>
-                        Additional Principle:
+                        <img src="img/loan_summary/extrapayment.png" alt="extra payment">
                     </div>
                     <div>
                         <?php echo $extra_payment_f; ?></div>
                     <div>
-                        Total Amount Paid:
+                        <img src="img/loan_summary/totalpaid.png" alt="total amount paid">
                     </div>
                     <div>
                         <?php echo $total_amt_f; ?></div>
                     <div>
-                        Interest Saved:
+                        <img src="img/loan_summary/interestsaved.png" alt="interest saved">
                     </div>
                     <div>
                         <?php echo $interest_saved_f ?></div>
+                        <div>
+                        <img src="img/loan_summary/monthssaved.png" alt="months saved">
+                    </div>
+                    <div>
+                        <?php echo $months_saved_f; ?></div>
                 </div>
             </div>
         </main>
-        <?php include '../../footer.php'; ?>
+        <?php include 'includes/footer.php'; ?>
     </body>
 </html>
--- index.php
+++ index.php
@@ -1,58 +1,93 @@
 <?php 
-    //set default value of variables for initial page load
-    if (!isset($loan_amount)) { $loan_amount = ''; } 
-    if (!isset($interest_rate)) { $interest_rate = ''; } 
-    if (!isset($loan_length)) { $loan_length = ''; }  
-?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Loan Calculator</title>
-    <link rel="stylesheet" type="text/css" href="styles/main.css">
-  </head>
-  <body>
-    <header>
-      <img src="img/loancalc.png" alt="Loan Calculator">
-    </header>
-    <main>
-      <?php if (!empty($error_message)) { ?>
-      <p class="error">
-        <?php echo htmlspecialchars($error_message); ?>
-      </p>
-      <?php } ?>
-      <form class="myForm" action="display_results.php" method="post">
-        <label><div class="tooltip">Loan Amount:
-  <span class="tooltiptext">If this is a new loan, this number will be the total amount you are borrowing. If you have already made payments,
-  
-   look at your current statement, and enter the remaining amount of principle owed after the last payement.</span>
-</div><span class="dollar_sign">$</span></label>
+ini_set('display_errors', 1);
 
+//set default value of variables for initial page load
+if (!isset($loan_amount)) { $loan_amount = ''; } 
+if (!isset($interest_rate)) { $interest_rate = ''; } 
+if (!isset($loan_length)) { $loan_length = ''; }  
+if (!isset($extra_payment)) { $extra_payment = ''; }  
+
+/* include header */
+
+include 'includes/header.php'; 
+
+?>
+<main>
+
+    <!-- error message -->
+    
+    <?php if (!empty($error_message)) { ?>
+    <p class="error">
+        <?php echo htmlspecialchars($error_message); ?>
+    </p>
+    <?php } ?>
+    
+    <!-- form -->
+    
+    <form class="myForm" action="display_results.php" method="post">
+    
+        <!-- Enter Loan Amount -->
+        
+        <label>
+        <div class="tooltip">
+            <img src="img/loan_summary/loanamount.png" align="left" alt="loan amount">
+            <span class="tooltiptext">This number will be the total amount you are borrowing.</span>
+        </div>
+            <span class="dollar_sign"><img src="img/index/dollarsign.png" alt="dollar sign"></span>
+        </label>
         <input type="text" name="loan_amount" value="<?php echo htmlspecialchars($loan_amount); ?>" placeholder="How much?">
-        <label><div class="tooltip">Interest Rate:
-  <span class="tooltiptext">Your interest rate is typically stated as a yearly rate, such as 5.5%.</span>
-</div></label>
+
+        <!-- Enter Interest Rate -->
+        
+        <label>
+        <div class="tooltip">
+            <img src="img/loan_summary/interestrate.png" align="left" alt="interest rate">
+            <span class="tooltiptext">Your interest rate is typically stated as a yearly rate, such as 5.5%.</span>
+        </div>
+        </label>
         <input type="text" class="interest_rate" name="interest_rate" value="<?php echo htmlspecialchars($interest_rate); ?>" placeholder="% interest rate">
-        <label><div class="tooltip">Loan Length:
-  <span class="tooltiptext">This is the full length of your loan in years or months. If this is an existing loan, subtract the number of payments you have already made from the original number of payments.</span>
-</div></label>
+
+        <!-- Enter Loan Length -->
+        
+        <label>
+        <div class="tooltip">
+            <img src="img/loan_summary/loanlength.png" align="left" alt="loan length">
+            <span class="tooltiptext">This is the full length of your loan in years or months. Enter whole years, or you may break it down to partial years and enter it as total months. 
+            For instance, 10 years, 6 months would be 126 months (10 x 12 plus 6).</span>
+        </div>
+        </label>
         <input type="text" name="loan_length" placeholder="How long?" value="<?php echo htmlspecialchars($loan_length); ?>">
-        <label><div class="tooltip">Years or Months:
-  <span class="tooltiptext">If entering years, enter whole years. If you need to break it down to partial years, enter it as months. For
-  
-   instance, 10 1/2 years would be 12 x 10 = 120 months plus another 6 months, for a total of 126 months.</span>
-</div></label>
+
+        <!-- Select Years or months -->
+        
+        <label>
+        <div class="tooltip">
+            <img src="img/loan_summary/yearsmonths.png" align="left" alt="choose years or months">
+            <span class="tooltiptext">Select years or months.</span>
+        </div>
+        </label>
         <select name="time_unit" id="time_unit">
-    <option value="years" selected="selected">Years</option>
-    <option value="months" >Months</option>
-  </select>
-        <label><div class="tooltip">Additional Principle: 
-  <span class="tooltiptext">If you intend to add an additional principle payment each month, enter that amount here, otherwise, leave it blank. Additional principle payments will result in significant savings over the life of your loan.</span>
-</div><span class="dollar_sign">&nbsp; &nbsp;$</span></label>
-        <input type="text" name="extra_payment" placeholder="Pay if off faster!" value="<?php echo htmlspecialchars($extra_pmt); ?>"> 
-        <button>Calculate</button>
-      </form>
-	  
-      <?php include '../../footer.php' ?>
-      </main>
- </body>
+            <option value="years" selected="selected">Years</option>
+            <option value="months" >Months</option>
+        </select>
+        
+        <!-- Enter Optional Extra Payment -->
+        
+        <label>
+        <div class="tooltip">
+            <img src="img/loan_summary/extrapayment.png" align="left" alt="additional principle">
+            <span class="tooltiptext">If you intend to add an additional principle payment each month, enter that amount here, otherwise, leave it blank. 
+                Additional principle payments will result in significant savings over the life of your loan.</span>
+        </div><span class="dollar_sign">&nbsp; &nbsp;<img src="img/index/dollarsign.png" alt="dollar sign"></span>
+        </label>
+        <input type="text" name="extra_payment" placeholder="Pay if off faster!" value="<?php echo htmlspecialchars($extra_payment); ?>">
+        <button><img src="img/index/calculate1.png"></button>
+    </form>
+    
+    <!-- Include footer -->
+    
+    <?php include 'includes/footer.php' ?>
+    
+</main>
+</body>
 </html>
